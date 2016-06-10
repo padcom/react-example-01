de37b2a2e06d81158ddcaa32a42a569f01222202
Matthias Hryniszak <padcom@gmail.com>
2016-06-10 13:37:54 +0200

    Tought the bower-utils to handle packages installed from URL that don't have an explicit bower.json

--- src/build/bower-utils.js
+++ src/build/bower-utils.js
@@ -1,9 +1,23 @@
 var argv = require('yargs').argv;
 var _ = require('lodash');
+var fs = require('fs');
 
 function unloadModule(module) {
   var name = require.resolve(module);
   delete require.cache[name];
+}
+
+function loadBowerManifest(module) {
+  const pathToModuleManifest = __dirname + '/' + '../../bower_components/' + module + '/';
+  if (fs.existsSync(pathToModuleManifest + 'bower.json')) {
+    unloadModule(pathToModuleManifest + 'bower.json');
+    return require(pathToModuleManifest + 'bower.json');
+  } else if (fs.existsSync(pathToModuleManifest + '.bower.json')) {
+    unloadModule(pathToModuleManifest + '.bower.json');
+    return require(pathToModuleManifest + '.bower.json');
+  } else {
+    throw "Module " + module + " has no bower manifest";
+  }
 }
 
 function getBowerPackageIds() {
@@ -19,16 +33,17 @@ function getBowerPackageIds() {
 
 function getBowerMains(module) {
   function makeMinFileName(file) {
+    const min = './bower_components/' + module + '/' + file.substr(0, file.indexOf('.js')) + '.min.js';
+    const reg = './bower_components/' + module + '/' + file;
     const env = process.env.NODE_ENV || 'development';
-    if (env === 'production' || !argv.debug) {
-      return './bower_components/' + module + '/' + file.substr(0, file.indexOf('.js')) + '.min.js';
+    if ((env === 'production' || !argv.debug) && fs.existsSync(__dirname + '/../../' + min)) {
+      return min;
     } else {
-      return './bower_components/' + module + '/' + file;
+      return reg;
     }
   }
 
-  unloadModule('../../bower_components/' + module + '/bower.json');
-  var manifest = require('../../bower_components/' + module + '/bower.json');
+  var manifest = loadBowerManifest(module);
   if (typeof manifest.main === 'string')
     return [ makeMinFileName(manifest.main) ]
   else
@@ -42,8 +57,7 @@ function getBowerModule(module) {
     return file.substr(0, file.indexOf('.'));
   }
 
-  unloadModule('../../bower_components/' + module + '/bower.json');
-  var manifest = require('../../bower_components/' + module + '/bower.json');
+  var manifest = loadBowerManifest(module);
   if (typeof manifest.main === 'string')
     return [ extractFileWithoutExt(manifest.main) ];
   else
